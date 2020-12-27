6b6b99faa09244e440a7e1aebfcf731708a282da
kocetora <kocetora@gmail.com>
2020-12-27 04:24:29 +0200

    added tier page, data interfaces and deck page wip

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/introduction.dart';
 import 'package:flutter_app/components/text.dart';
 import 'package:flutter_app/pages/observations.dart';
-
+import 'package:flutter_app/pages/tier.dart';
+import 'package:flutter_app/data/deck.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -24,12 +25,12 @@ class HomePage extends StatelessWidget {
             children: [
               textWidgets.logo(),
               textWidgets.redirectButton('Introduction', IntroductionPage(), context),
-              textWidgets.redirectButton('Observations',ObservationsPage(), context),
+              textWidgets.redirectButton('Observations', ObservationsPage(), context),
               textWidgets.redirectButton('Deck trends', IntroductionPage(), context), //change page
-              textWidgets.redirectButton('Tier1', IntroductionPage(), context), //change page
-              textWidgets.redirectButton('Tier2', IntroductionPage(), context), //change page
-              textWidgets.redirectButton('Tier3', IntroductionPage(), context), //change page
-              textWidgets.redirectButton('Tier4', IntroductionPage(), context), //change page
+              textWidgets.redirectButton('Tier1', TierPage(decks: decks.where((deck) => deck.tier == 1).toList()), context),
+              textWidgets.redirectButton('Tier2', TierPage(decks: decks.where((deck) => deck.tier == 2).toList()), context),
+              textWidgets.redirectButton('Tier3', TierPage(decks: decks.where((deck) => deck.tier == 3).toList()), context),
+              textWidgets.redirectButton('Tier4', TierPage(decks: decks.where((deck) => deck.tier == 4).toList()), context),
             ]
         ),
       ),
