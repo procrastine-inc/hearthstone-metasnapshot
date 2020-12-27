1dd32db0812ad67da80ea8b4fae5360e4893947e
kocetora <kocetora@gmail.com>
2020-12-27 06:55:45 +0200

    deck page ready

--- lib/pages/introduction.dart
+++ lib/pages/introduction.dart
@@ -2,7 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/components/text.dart';
 
 //delete this in future
-const String usage = "This tier list shows the best decks to play in Ranked mode to maximize the chances of winning the game and climbing the ladder.";
+const String usage =
+'''This tier list shows the best decks to play in Ranked mode to maximize the 
+chances of winning the game and climbing the ladder.
+
+Click on the name of the archetype in each tier to expand more details about the deck.
+
+The "Archetype Explanation" section gives a general explanation of the archetype 
+as a whole. It will present playstyle strategies, discuss different variants of 
+the deck, and help you identify the deck on ladder. The "Weekly Meta" section 
+analyzes the role of the deck in the current week's meta, and focuses in on 
+the nuances of playing the current week's featured deck variant.
+
+The "Tech Decision" section suggests card substitution options that you may 
+consider if you are frequently facing a particular type of deck on ladder (i.e., 
+you are being swarmed by aggro, or constantly queue into control decks). The 
+"Match-ups" section gives an approximate average percentage chance that you 
+will win a game when facing another Tier 1 or Tier 2 deck.
+
+Click on the "View Deck" button to view in-depth information about the deck, 
+such as the card composition, mana curve, mulligans, and more. The date in the 
+title of the deck is the date on which that variant of the deck was first posted 
+on the Meta Snapshot. If the construction of a deck has not changed since then, 
+the deck may be reused in a future Snapshot. Make sure to scroll down to read 
+the latest discussion on the deck's current role in the meta.
+''';
+
+const String legend =
+'''Tier S ("God Tier"): Overtuned decks that warp and control a large portion of 
+the meta. (This tier may not always be present in the Snapshot if there are no 
+overpoweringly imbalanced decks.)
+Tier 1: Well-optimized decks with extremely efficient and overwhelmingly powerful 
+combos and card synergies that makes losing against these decks feel helpless 
+and unfair.
+Tier 2: Competitive decks that have a few slight weaknesses (e.g., poor comeback 
+mechanics or draw consistency); can still take games off top-tier decks with 
+tech switches.
+Tier 3: Average decks that aren't bad, but also aren't optimized or refined; 
+decks that have styles not currently favored by the metagame.
+Tier 4: Inconsistent decks that are unrefined, out-of-flavor, overly niche, or 
+retired. Taking wins off upper-tier decks requires an intimate understanding of 
+the role of every card in the deck.
+Tier 5: Fun decks that should only be used if you play Hearthstone for the joy 
+of the gameplay, rather than the joy of winning.
+''';
+
+const update =
+'''The next Hearthstone Standard Meta Snapshot will be published on or before 
+Dec. 29, 2020.
+''';
 
 class IntroductionPage extends StatelessWidget {
 
@@ -12,14 +60,20 @@ class IntroductionPage extends StatelessWidget {
         appBar: AppBar(
           title: Text("HSmeta"),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              textWidgets.title('Introduction', 5, 10, 30),
-              textWidgets.title('HOW TO USE THE META SNAPSHOT', 5, 5, 20),
-              Text(usage, style: TextStyle(fontSize: 18))
-            ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                textWidgets.title('Introduction', 5, 10, 30),
+                textWidgets.title('HOW TO USE THE META SNAPSHOT', 5, 5, 20),
+                Text(usage, style: TextStyle(fontSize: 18)),
+                textWidgets.title('LEGEND:', 5, 5, 20),
+                Text(legend, style: TextStyle(fontSize: 18)),
+                textWidgets.title('NEXT UPDATE', 5, 5, 20),
+                Text(update, style: TextStyle(fontSize: 18)),
+              ],
+            ),
           ),
         )
     );
--- lib/pages/tier.dart
+++ lib/pages/tier.dart
@@ -4,30 +4,33 @@ import 'package:flutter_app/pages/deck.dart';
 
 class TierPage extends StatelessWidget {
   final List<Deck> decks;
+  final String tier;
 
-  TierPage({Key key, @required this.decks}) : super(key: key);
+  TierPage({Key key, @required this.decks, @required this.tier}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tier1'),
+        title: Text('Tier' + tier),
       ),
       body: ListView.builder(
         itemCount: decks.length,
         itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(decks[index].name),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DeckPage(deck: decks[index]),
-                  ),
-                );
-              },
-            )
+          return SingleChildScrollView(
+            child: Card(
+                child: ListTile(
+                  title: Text(decks[index].name),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DeckPage(deck: decks[index]),
+                      ),
+                    );
+                  },
+                )
+            ),
           );
         },
       ),
