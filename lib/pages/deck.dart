04ba2412203d85882336a663e3aecdee9a9981d2
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-12-28 11:46:47 +0200

    Fixed the chart overflow to the right

--- lib/pages/deck.dart
+++ lib/pages/deck.dart
@@ -93,12 +93,16 @@ class DeckPage extends StatelessWidget {
                       textWidgets.text(deck.description, context),
                       textWidgets.paragraph('Matchups:', context),
                       deck.matchUps.length > 0 ?
-                      Container(
-                          alignment: Alignment.center,
-                          width: 350, height: 350,
-                          child: CustomRoundedBars(
-                              transformMatchUp(deck.matchUps),
-                              animate: false)
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Container(
+                            alignment: Alignment.center,
+                            width: 350, height: 350,
+                            child: Expanded(child: CustomRoundedBars(
+                                transformMatchUp(deck.matchUps),
+                                animate: false)
+                            )
+                        )
                       ) : textWidgets.text('No matchups available',context),
                       textWidgets.paragraph('Cards:',context),
                       CardsComponent(cards: deck.cards)
